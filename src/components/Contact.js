5d642dd9dafeb5579581533a13d5a7f84b9d635b
Cody Crandall <cody@halfhelix.com>
2022-01-30 21:27:53 -0600

    add contact and about id

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Translate } from "react-translated";
 
 const Contact = () => (
-  <>
+  <div id="contact">
     <h3 className="text-5xl text-center">
       <Translate text="contact" />
     </h3>
@@ -11,7 +11,7 @@ const Contact = () => (
     </p>
     <p className="text-2xl">Snapchat:</p>
     <p className="text-2xl">Email: bobloblaw@gmail.com</p>
-  </>
+  </div>
 );
 
 export default Contact;
